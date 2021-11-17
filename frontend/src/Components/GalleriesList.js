159902937866242e5a1ed18b1d73fd50bc42b15d
Kelvin Fundi Njagi <38814636+KevinLahm@users.noreply.github.com>
2021-11-17 20:37:18 +0300

    Removed component css
    Removed component css

--- frontend/src/Components/GalleriesList.js
+++ frontend/src/Components/GalleriesList.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { fetchGalleries} from '../Actions/galleriesActions';
 import { connect } from 'react-redux';
-import "./GalleriesList.css";
 import Gallery from './Gallery';
 
 class GalleriesList extends React.Component {
